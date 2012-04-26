2a42cfe77a50d82c6ddee117416880ea3688a972
David Kanenwisher <david.kanenwisher@bolderthinking.com>
2012-04-25 21:09:51 -0500

    1. moved the tcp server with recv into it's own file. It doesn't appear to be the right way to do things, interesting none-the-less.

--- tcp/serverrecv.pl
+++ tcp/serverrecv.pl
@@ -1,4 +1,13 @@
 use IO::Socket;
+use Getopt::Long;
+
+my $length = 1024;
+my $limit  = 100;
+
+$option = GetOptions ("length=i" => \$length
+                     ,"limit=i"  => \$limit
+                     );
+
 my $sock = new IO::Socket::INET ( LocalHost => '127.0.0.1'
                                 , LocalPort => '7070'
                                 , Proto => 'tcp'
@@ -7,10 +16,12 @@ my $sock = new IO::Socket::INET ( LocalHost => '127.0.0.1'
                                 );
 die "Could not create socket: $!\n" unless $sock;
 
-while(1)
+$count = 0;
+
+while($count < $limit)
 {
-    sleep 5;
-    $sock->recv($received_data, (1024 * 100));
-    print "length: " . length($received_data) . "\n";
+    $sock->recv($received_data, ($length));
+    print "[$count]length: " . length($received_data) . "\n";
+    $count++;
 }
 close $sock;
